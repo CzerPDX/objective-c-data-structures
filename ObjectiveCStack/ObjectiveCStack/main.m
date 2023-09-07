55f1e04498987d70eaf43425d4b9e38d8efcc411
czer <czerpdx@gmail.com>
2023-09-07 15:16:06 -0700

    Initial empty Xcode command line project commit

---
+++ ObjectiveCStack/ObjectiveCStack/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveCStack
+//
+//  Created by Brooke Czerwinski on 9/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
