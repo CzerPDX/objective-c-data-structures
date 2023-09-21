4571a19cd1de18b65791ea43c8dc971780a4b114
czer <czerpdx@gmail.com>
2023-09-21 16:14:08 -0700

    supplemented add and delete test for binary tree with random data add and delete

--- ObjectiveCDataStructuresTests/BaseBinaryTreeTests.m
+++ ObjectiveCDataStructuresTests/BaseBinaryTreeTests.m
@@ -47,11 +47,14 @@
     [comparisonData addObject:testData1];
     NSNumber *testData2 = @2;
     [comparisonData addObject:testData2];
+    NSNumber *testData3 = @3;
+    [comparisonData addObject:testData3];
     
     // Then add the data to the tree
     // Add lower-numbered ID data first, followed by a higher ID
+    [self.testBinaryTree addObjectByID:2 andObjectData:testData2];
     [self.testBinaryTree addObjectByID:1 andObjectData:testData1];
-    XCTAssertEqual([self.testBinaryTree addObjectByID:2 andObjectData:testData2], SuccessfullyAdded, @"Adding data to a non-empty tree did not succeed.");
+    XCTAssertEqual([self.testBinaryTree addObjectByID:3 andObjectData:testData3], SuccessfullyAdded, @"Adding data to a non-empty tree did not succeed.");
     
     // Then, verify that the tree contains the same objects in the comparisonData array
     XCTAssertTrue([comparisonData isEqualToArray:[self.testBinaryTree returnDataInOrder]], @"Data not added to tree correctly.");
@@ -65,9 +68,12 @@
     [comparisonData addObject:testData1];
     NSNumber *testData2 = @2;
     [comparisonData addObject:testData2];
+    NSNumber *testData3 = @3;
+    [comparisonData addObject:testData3];
     
     // Then add the data to the tree
     // Add higher-numbered ID data first, followed by a lower ID
+    [self.testBinaryTree addObjectByID:3 andObjectData:testData3];
     [self.testBinaryTree addObjectByID:2 andObjectData:testData2];
     XCTAssertEqual([self.testBinaryTree addObjectByID:1 andObjectData:testData1], SuccessfullyAdded, @"Adding data to a non-empty tree did not succeed.");
     
@@ -105,6 +111,44 @@
     XCTAssertEqual([self.testBinaryTree addObjectByID:42 andObjectData:@"Duplicate ID data"], FailedAddDuplicate, @"Adding duplicate ID to tree should fail with FailedAddDuplicate type, but it did not.");
 }
 
+
+- (void)helperAddRandomDataInRange:(int)randomNumberRangeCap forRandomNumberOfRoundsRange:(int)numberOfRoundsCap andUpdateComparisonDataArray:(NSMutableArray *)comparisonData {
+    
+    NSInteger numberOfRounds = arc4random_uniform(numberOfRoundsCap);
+    
+    for (int i = 0; i < numberOfRounds; i++) {
+        // Create a random number to save as data
+        NSInteger randomNumber = arc4random_uniform(randomNumberRangeCap);
+        
+        // Add the random number to the tree
+        AddResult addResult = [self.testBinaryTree addObjectByID:randomNumber andObjectData:@(randomNumber)];
+        
+        // Only add to the comparisonData array if it was successfully added to the tree (no duplicates)
+        if (addResult == SuccessfullyAdded) {
+            [comparisonData addObject:@(randomNumber)];
+        }
+    }
+}
+
+
+// Adding tests for all test cases would be extensive. To supplement basic testing, I've created a random add test that will randomly add data to the tree.
+// After being added, it compares the inorder output of the tree's data to the sorted data that was added.
+// The sorted comparison data and the inorder data output from the tree should match. If not, then the original order of the data is output in the test failure message so it can be replicated.
+- (void)testRandomAdd {
+    NSMutableArray *comparisonData = [[NSMutableArray alloc] init];
+    
+    // Call the helper function to add random data to the tree
+    [self helperAddRandomDataInRange:2000 forRandomNumberOfRoundsRange:100 andUpdateComparisonDataArray:comparisonData];
+    
+    
+    // Sort the comparison data and get the sorted data from the tree
+    NSArray *sortedComparisonData = [comparisonData sortedArrayUsingSelector:@selector(compare:)];
+    NSMutableArray *inorderBinaryTreeData = [self.testBinaryTree returnDataInOrder];
+    
+    // Compare the comparison data to the inorder data returned from the tree
+    XCTAssertTrue([sortedComparisonData isEqualToArray:inorderBinaryTreeData], @"Data not added to tree correctly. \n\nOriginal Data added: \n%@ \n\n Output from binary tree: \n%@", [comparisonData description], [inorderBinaryTreeData description]);
+}
+
 // Get data at object ID
 // Get data at objectID when tree is empty
 - (void)getDataAtObjectIDWhenTreeIsEmpty {
@@ -124,35 +168,47 @@
 }
 
 // Get data at objectID when tree is not empty and the data exists and is the highest-value id in the tree
-- (void)getDataAtObjectIDWhenIDExistsAndIsLowestValueID {
+- (void)getDataAtObjectIDWhenIDExistsAndIsLowestValue {
     NSString *testDataLowest = @"Lowest test data";
     NSInteger testIDLowest = 1;
+    NSString *testDataLower = @"Lower test data";
+    NSInteger testIDLower = 2;
     NSString *testDataMedium = @"Mediumest test data";
-    NSInteger testIDMedium = 2;
+    NSInteger testIDMedium = 3;
+    NSString *testDataHigher = @"Higher test data";
+    NSInteger testIDHigher = 4;
     NSString *testDataHighest = @"Highest test data";
-    NSInteger testIDHighest = 42;
+    NSInteger testIDHighest = 5;
     
     // Add the data to the tree
     [self.testBinaryTree addObjectByID:testIDLowest andObjectData:testDataLowest];
+    [self.testBinaryTree addObjectByID:testIDLower andObjectData:testDataLower];
     [self.testBinaryTree addObjectByID:testIDMedium andObjectData:testDataMedium];
+    [self.testBinaryTree addObjectByID:testIDHigher andObjectData:testDataHigher];
     [self.testBinaryTree addObjectByID:testIDHighest andObjectData:testDataHighest];
     
     // Search for the data
     XCTAssertEqual([self.testBinaryTree getDataAtObjectID:testIDLowest], testDataLowest, @"Test data did not match expected search outcome.");
 }
 
-// Get data at objectID when tree is not empty and the data exists and is the highest-value id in the tree
-- (void)getDataAtObjectIDWhenIDExistsAndIsNeitherHigherNorLowestValueID {
+// Get data at objectID when tree is not empty and the data exists and is neither the highest, nor lowest, id in the tree
+- (void)getDataAtObjectIDWhenIDExistsAndIsNeitherHigherNorLowestValue {
     NSString *testDataLowest = @"Lowest test data";
     NSInteger testIDLowest = 1;
+    NSString *testDataLower = @"Lower test data";
+    NSInteger testIDLower = 2;
     NSString *testDataMedium = @"Mediumest test data";
-    NSInteger testIDMedium = 2;
+    NSInteger testIDMedium = 3;
+    NSString *testDataHigher = @"Higher test data";
+    NSInteger testIDHigher = 4;
     NSString *testDataHighest = @"Highest test data";
-    NSInteger testIDHighest = 42;
+    NSInteger testIDHighest = 5;
     
     // Add the data to the tree
     [self.testBinaryTree addObjectByID:testIDLowest andObjectData:testDataLowest];
+    [self.testBinaryTree addObjectByID:testIDLower andObjectData:testDataLower];
     [self.testBinaryTree addObjectByID:testIDMedium andObjectData:testDataMedium];
+    [self.testBinaryTree addObjectByID:testIDHigher andObjectData:testDataHigher];
     [self.testBinaryTree addObjectByID:testIDHighest andObjectData:testDataHighest];
     
     // Search for the data
@@ -160,17 +216,23 @@
 }
 
 // Get data at objectID when tree is not empty and the data exists and is the highest-value id in the tree
-- (void)getDataAtObjectIDWhenIDExistsAndIsHighestValueID {
+- (void)getDataAtObjectIDWhenIDExistsAndIsHighestValue {
     NSString *testDataLowest = @"Lowest test data";
     NSInteger testIDLowest = 1;
+    NSString *testDataLower = @"Lower test data";
+    NSInteger testIDLower = 2;
     NSString *testDataMedium = @"Mediumest test data";
-    NSInteger testIDMedium = 2;
+    NSInteger testIDMedium = 3;
+    NSString *testDataHigher = @"Higher test data";
+    NSInteger testIDHigher = 4;
     NSString *testDataHighest = @"Highest test data";
-    NSInteger testIDHighest = 42;
+    NSInteger testIDHighest = 5;
     
     // Add the data to the tree
     [self.testBinaryTree addObjectByID:testIDLowest andObjectData:testDataLowest];
+    [self.testBinaryTree addObjectByID:testIDLower andObjectData:testDataLower];
     [self.testBinaryTree addObjectByID:testIDMedium andObjectData:testDataMedium];
+    [self.testBinaryTree addObjectByID:testIDHigher andObjectData:testDataHigher];
     [self.testBinaryTree addObjectByID:testIDHighest andObjectData:testDataHighest];
     
     // Search for the data
@@ -181,84 +243,92 @@
 - (void)getDataAtObjectIDWhenIDDoesNotExist {
     NSString *testDataLowest = @"Lowest test data";
     NSInteger testIDLowest = 1;
+    NSString *testDataLower = @"Lower test data";
+    NSInteger testIDLower = 2;
     NSString *testDataMedium = @"Mediumest test data";
-    NSInteger testIDMedium = 2;
+    NSInteger testIDMedium = 3;
+    NSString *testDataHigher = @"Higher test data";
+    NSInteger testIDHigher = 4;
     NSString *testDataHighest = @"Highest test data";
-    NSInteger testIDHighest = 3;
+    NSInteger testIDHighest = 5;
     NSInteger testIDDoesntExist = 42;
     
     // Add the data to the tree
-    [self.testBinaryTree addObjectByID:testIDLowest andObjectData:testDataLowest];
     [self.testBinaryTree addObjectByID:testIDMedium andObjectData:testDataMedium];
     [self.testBinaryTree addObjectByID:testIDHighest andObjectData:testDataHighest];
+    [self.testBinaryTree addObjectByID:testIDHigher andObjectData:testDataHigher];
+    [self.testBinaryTree addObjectByID:testIDLowest andObjectData:testDataLowest];
+    [self.testBinaryTree addObjectByID:testIDLower andObjectData:testDataLower];
     
-    // Search for the data
+    // Search for the data that doesn't exist
     XCTAssertNil([self.testBinaryTree getDataAtObjectID:testIDDoesntExist], @"Searching for test ID that has not added should have resulted in nil.");
 }
 
 // Delete object by ID
 // Delete from an empty tree
-- (void)testDeleteObjectByIDWhenTreeEmpty {
+- (void)testDeleteObjectByIDWhenTreeIsEmpty {
     XCTAssertEqual([self.testBinaryTree deleteObjectByID:42], SuccessfullyDeletedNotFound, @"Deleting from tree when tree is empty should return SuccessfullyDeletedNotFound but it did not.");
 }
 
-// Delete from a tree where the item exists with no children
-- (void)testDeleteObjectByIDWhenIDExistsAlone {
+// Delete from a tree where the item exists at root with no children
+- (void)testDeleteObjectByIDWhenIDExists {
     // Add data to remove
     [self.testBinaryTree addObjectByID:42 andObjectData:@"Some test data"];
     
     // Verify that deleting it is successful
     XCTAssertEqual([self.testBinaryTree deleteObjectByID:42], SuccessfullyDeleted, @"Deleting from tree did not return SuccessfullyDeleted.");
+    
 }
 
-// Delete from a tree where the item to delete was added first and then
-- (void)testDeleteObjectByIDWhenIDWasAddedFirstThenLowerIDsAdded {
-    // First, add the data to remove
-    [self.testBinaryTree addObjectByID:42 andObjectData:@"Some test data"];
-    // Then add some lower-numbered ID data
-    [self.testBinaryTree addObjectByID:16 andObjectData:@"Some lower-numbered test data"];
-    [self.testBinaryTree addObjectByID:11 andObjectData:@"Some more lower-numbered test data"];
+// Delete from a tree where the item does not exist
+- (void)testDeleteObjectByIDWhenIDDoesNotExist {
+    // Add data to the tree
+    [self.testBinaryTree addObjectByID:1 andObjectData:@"Some more test data"];
+    [self.testBinaryTree addObjectByID:3 andObjectData:@"Even more test data"];
+    [self.testBinaryTree addObjectByID:2 andObjectData:@"Even even more test data"];
     
-    // Then verify that deleting the original data is successful
-    XCTAssertEqual([self.testBinaryTree deleteObjectByID:42], SuccessfullyDeleted, @"Deleting from tree did not return SuccessfullyDeleted.");
+    // Try to delete an objectID that hasn't been added (should be SuccessfullyDeletedNotFound)
+    XCTAssertEqual([self.testBinaryTree deleteObjectByID:42], SuccessfullyDeletedNotFound, @"Deleting non-existent item from the tree did not return SuccessfullyDeletedNotFound.");
 }
 
-// Delete from a tree where the item exists at the root with a left child which itself does not have any children
-- (void)testDeleteObjectByIDWhenIDWasAddedFirstThenHigherIDsAdded {
-    // First, add the data to remove
-    [self.testBinaryTree addObjectByID:42 andObjectData:@"Some test data"];
-    // Then add some higher-numbered ID data
-    [self.testBinaryTree addObjectByID:50 andObjectData:@"Some higher-numbered test data"];
-    [self.testBinaryTree addObjectByID:51 andObjectData:@"Some more higher-numbered test data"];
+// This is a random test function meant to supplement testing of the delete function.
+// It interleaves adding data manually that is not added to comparisonData array so it can be removed
+// Comparison data without data to remove is sorted and compared to inorder data output from the tree. They should match or the test fails.
+- (void)testRandomAddDelete {
+    NSMutableArray *comparisonData = [[NSMutableArray alloc] init];
     
-    // Then verify that deleting the original data is successful
-    XCTAssertEqual([self.testBinaryTree deleteObjectByID:42], SuccessfullyDeleted, @"Deleting from tree did not return SuccessfullyDeleted.");
-}
-
-// Delete from a tree where the item exists at the root both left and right children which themselves do not have any children
-- (void)testDeleteObjectByIDWhenItemExistsAtRootWithBothChildrenAsLeaves {
-    // Add the data to the root
-    [self.testBinaryTree addObjectByID:42 andObjectData:@"Some test data"];
-    // Add data to the right child
-    [self.testBinaryTree addObjectByID:50 andObjectData:@"Some test data for right child"];
-    // Add data to the left child
-    [self.testBinaryTree addObjectByID:16 andObjectData:@"Some test data for left child"];
+    // Add some data manually before removing anything (do not add to comparisonData)
+    [self.testBinaryTree addObjectByID:239 andObjectData:@(239)];
+    [self.testBinaryTree addObjectByID:1571 andObjectData:@(1571)];
     
-    // Verify that deleting the root is successful
-    XCTAssertEqual([self.testBinaryTree deleteObjectByID:42], SuccessfullyDeleted, @"Deleting from tree when tree is empty should return SuccessfullyDeletedNotFound but it did not.");
-}
-
-// Delete from a tree where the item exists at the root with a left child which itself has a left child
-- (void)testDeleteObjectByIDWhenItemExistsAtRootWithLeftChild {
-    // Add the data to the root
-    [self.testBinaryTree addObjectByID:42 andObjectData:@"Some test data"];
-    // Add data to the left child
-    [self.testBinaryTree addObjectByID:16 andObjectData:@"Some test data for left child"];
-    // Add data to the left child
-    [self.testBinaryTree addObjectByID:16 andObjectData:@"Some test data for left child"];
+    // Then run the helper function (round 1)
+    [self helperAddRandomDataInRange:2000 forRandomNumberOfRoundsRange:50 andUpdateComparisonDataArray:comparisonData];
     
-    // Verify that deleting the root is successful
-    XCTAssertEqual([self.testBinaryTree deleteObjectByID:42], SuccessfullyDeleted, @"Deleting from tree when tree is empty should return SuccessfullyDeletedNotFound but it did not.");
+    // Then add more data manually (do not add to comparisonData)
+    [self.testBinaryTree addObjectByID:181 andObjectData:@(181)];
+    [self.testBinaryTree addObjectByID:1559 andObjectData:@(1559)];
+    
+    // Then run helper function (round 2)
+    [self helperAddRandomDataInRange:2000 forRandomNumberOfRoundsRange:50 andUpdateComparisonDataArray:comparisonData];
+    
+    // Then add more data manually (do not add to comparisonData)
+    [self.testBinaryTree addObjectByID:42 andObjectData:@(42)];
+    [self.testBinaryTree addObjectByID:977 andObjectData:@(977)];
+    
+    // Then delete all removed data added manually
+    [self.testBinaryTree deleteObjectByID:239];
+    [self.testBinaryTree deleteObjectByID:1571];
+    [self.testBinaryTree deleteObjectByID:181];
+    [self.testBinaryTree deleteObjectByID:1559];
+    [self.testBinaryTree deleteObjectByID:42];
+    [self.testBinaryTree deleteObjectByID:977];
+    
+    // Sort the comparison data and get the sorted data from the tree
+    NSArray *sortedComparisonData = [comparisonData sortedArrayUsingSelector:@selector(compare:)];
+    NSMutableArray *inorderBinaryTreeData = [self.testBinaryTree returnDataInOrder];
+    
+    // Compare the comparison data to the inorder data returned from the tree
+    XCTAssertTrue([sortedComparisonData isEqualToArray:inorderBinaryTreeData], @"Data not removed from tree correctly. \n%@ \n\n Output from binary tree: \n%@", [comparisonData description], [inorderBinaryTreeData description]);
 }
 
 @end
--- ObjectiveCDataStructuresTests/UnbalancedBinaryTreeTests.m
+++ ObjectiveCDataStructuresTests/UnbalancedBinaryTreeTests.m
@@ -20,4 +20,50 @@
     self.testBinaryTree = [[UnbalancedBinaryTree alloc] init];
 }
 
+
+// Delete nodes from the unbalanced binary tree
+
+// Below I wrote out the tests that would need to happen to verify that deleting the root works in all scenarios. This is too extensive for me to undertake, so instead I will try to write a randomized test that can apply to all trees and focuses more on putting the tree though a bunch of scenarios and then verifying that it has the correct information afterward.
+// Delete root
+//      - When it has no children
+//      - When it has a left child
+//          - And left child is leaf
+//          - And left child has left child
+//          - And left child has right child
+//          - And left child has left and right child
+//      - When it has a right child
+//          - And right child is leaf
+//          - And right child has left child
+//          - And right child has right child
+//          - And right child has left and right child
+//      - When it has both left and right children
+//          - And both are leaves
+//          - Left child is leaf right child has children
+//              - Right child has only left child
+//              - Right child has only right child
+//              - Right child has both left and right child
+//              - Left child has only left child
+//              - Left child has only right child
+//              - Left child has both left and right child
+//          - Right child is a leaf and left child has children
+//              - Right child has only left child
+//              - Right child has only right child
+//              - Right child has both left and right child
+//              - Left child has only left child
+//              - Left child has only right child
+//              - Left child has both left and right child
+//          - Both left and right have children
+//              - Right has only left child
+//                  - Left has only left child
+//                  - Left has only right child
+//                  - Left has both left and right child
+//              - Right has only right child
+//                  - Left has only left child
+//                  - Left has only right child
+//                  - Left has both left and right child
+//              - Right has both left and right child
+//                  - Left has only left child
+//                  - Left has only right child
+//                  - Left has both left and right child
+
 @end
